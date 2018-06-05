f8a18003cc711d60ea9d934e812f9919b897aa4e
matthew.reid60 <matthew.reid60@600900J211DE404>
2018-06-05 15:56:19 +1000

    started the assessment on flappy bird

---
+++ Unity/Assets/Detect/Dectect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dectect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
